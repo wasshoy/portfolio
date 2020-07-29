3c1fe4bd1134efc5787a46a15d9ea7d9708825da
A.Arimura <404ari@gmail.com>
2020-07-30 08:25:20 +0900

    Updates

---
+++ precache-manifest.d062b04fa89fba4f091fa5c789b7abd3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4df714da0d72befc8049fbf0f65971",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e9c2a419844838dc63ea",
+    "url": "/portfolio/static/css/main.ddb5515d.chunk.css"
+  },
+  {
+    "revision": "dd583ff299ffd0af6fb2",
+    "url": "/portfolio/static/js/2.1a6e01bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1a6e01bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c2a419844838dc63ea",
+    "url": "/portfolio/static/js/main.06a77b6e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a3620c0b63573282ebcccca50fe4c7b0",
+    "url": "/portfolio/static/media/dpcomic.regular.a3620c0b.ttf"
+  },
+  {
+    "revision": "ace87566a523960287b20a3f9c064cb9",
+    "url": "/portfolio/static/media/fff-forward.regular.ace87566.ttf"
+  },
+  {
+    "revision": "52fb93dd5f86f24a6ca39931ae4f21db",
+    "url": "/portfolio/static/media/yurumoji.52fb93dd.ttf"
+  }
+]);
